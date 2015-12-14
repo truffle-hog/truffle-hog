8da97f1defce789ec638edcfe9b89fc7be82044f
Infinity95 <mark.giraud@t-online.de>
2015-12-14 22:49:19 +0100

    Set up project for gradle. Moved test to testProject folder.

---
+++ src/main/java/TruffleHog/TruffleHog.java
@@ -0,0 +1,12 @@
+package TruffleHog;
+
+/**
+ * @author Mark Giraud
+ * @version 0.0
+ */
+public class TruffleHog {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
