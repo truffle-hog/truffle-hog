7ab5d7939037f22e4eb70a7f71813b390f1b7152
Julian Brendl <julianbrendl@gmail.com>
2016-01-25 17:31:52 +0100

    added javadoc to tex script

---
+++ doc/create_doc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Run this from the doc directory
+
+sudo javadoc -docletpath TeXDoclet.jar -doclet org.stfm.texdoclet.TeXDoclet -sourcepath ./src/main/java -subpackages . -d ./doc
