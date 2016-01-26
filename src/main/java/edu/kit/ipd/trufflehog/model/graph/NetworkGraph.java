22e30f74bd400fdba3625b40272c60ae165223fa
Julian Brendl <julianbrendl@gmail.com>
2016-01-26 20:49:10 +0100

    started documenting datalogging

---
+++ src/main/java/edu/kit/ipd/trufflehog/model/graph/NetworkGraph.java
@@ -0,0 +1,7 @@
+package edu.kit.ipd.trufflehog.model.graph;
+
+/**
+ * Created by julianbrendl on 1/26/16.
+ */
+public class NetworkGraph {
+}
---
+++ src/main/java/edu/kit/ipd/trufflehog/service/datalogging/DataLogLoadService.java
@@ -0,0 +1,7 @@
+package edu.kit.ipd.trufflehog.service.datalogging;
+
+/**
+ * Created by julianbrendl on 1/26/16.
+ */
+public class DataLogLoadService {
+}
