939664232e7e5eb21e00d718f4656122c01fd2f5
Julian Brendl <julianbrendl@gmail.com>
2016-01-26 23:47:49 +0100

    finished CommandCompressor

--- src/main/java/de/fraunhofer/iosb/trufflehog/service/datalogging/CommandCompressor.java
+++ src/main/java/de/fraunhofer/iosb/trufflehog/service/datalogging/CommandCompressor.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 /**
  * <p>
- *
+ *     The CommandCompressor creates a compacted list of all commands it received by using the {@link CommandCompressor}.
+ *     For example, if there are 50 consecutive commands in the list that all increment the same counter by 1, these 50
+ *     commands are packaged into 1 command that increments the counter by 50 and then returned as a single command.
  * </p>
- *
- * @author MrX
  */
 public class CommandCompressor {
 
