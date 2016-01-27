1b6559d5d248c5215a5b3148bbb35e21c4920f1e
Julian Brendl <julianbrendl@gmail.com>
2016-01-27 10:35:17 +0100

    updated datalogging documentation

--- src/main/java/de/fraunhofer/iosb/trufflehog/service/datalogging/CommandCompressor.java
+++ src/main/java/de/fraunhofer/iosb/trufflehog/service/datalogging/CommandCompressor.java
@@ -16,9 +16,7 @@ public class CommandCompressor {
 
     /**
      * <p>
-     *     Creates a compacted list of all commands it received by using the {@link CommandCompressor}. For example, if
-     *     there are 50 consecutive commands in the list that all increment the same counter by 1, these 50 commands are
-     *     packaged into 1 command that increments the counter by 50 and then returned as a single command.
+     *     Creates a compacted list of all commands it received the way it has been described in the class description.
      * </p>
      *
      * @param commands The commands to compress.
