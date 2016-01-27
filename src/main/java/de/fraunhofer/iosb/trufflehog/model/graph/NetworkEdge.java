640442eacbd0b3be03a1540da6ceea0d7e2de93f
Hoehler <hoehler.d@web.de>
2016-01-27 11:32:33 +0100

    Added properties to NetworkEdge

--- src/main/java/de/fraunhofer/iosb/trufflehog/model/graph/NetworkEdge.java
+++ src/main/java/de/fraunhofer/iosb/trufflehog/model/graph/NetworkEdge.java
@@ -1,5 +1,6 @@
 package de.fraunhofer.iosb.trufflehog.model.graph;
 
+import javafx.beans.property.*;
 import java.io.Serializable;
 
 /**<p>
@@ -8,10 +9,88 @@ import java.io.Serializable;
  */
 public class NetworkEdge implements IConnection, Serializable {
 
-	private long totalPacketCount;
-	private int connectionType;
-	private long active;
+    private LongProperty totalPacketCount = new SimpleLongProperty();
+    private LongProperty active = new SimpleLongProperty();
+    private IntegerProperty connectionType = new SimpleIntegerProperty();
 
-    /*properties getter!
+    /**<p>
+     * Getter for the total packet count
+     * </p>
+     * @return total packet count
      */
+    public final long getTotalPacketCount() {
+        return totalPacketCount.get();
+    }
+
+    /**<p>
+     * Setter for the total packet count
+     * </p>
+     * @param value New packet count value
+     */
+    public final void setTotalPacketCount(long value) {
+        totalPacketCount.set(value);
+    }
+
+    /**<p>
+     * Getter for the total packet count property
+     * </p>
+     * @return total packet property
+     */
+    public LongProperty getTotalPacketCountProperty() {
+        return totalPacketCount;
+    }
+
+    /**<p>
+     * Getter for the period of activity
+     * </p>
+     * @return period of activity
+     */
+    public final long getActive() {
+        return active.get();
+    }
+
+    /**<p>
+     * Setter for the period of activity
+     * </p>
+     * @param value set new period of activity
+     */
+    public final void setActive(long value) {
+        active.set(value);
+    }
+
+    /**<p>
+     * Getter for the period of activity property
+     * </p>
+     * @return period of activity property
+     */
+    public LongProperty getActiveProperty() {
+        return active;
+    }
+
+    /**<p>
+     * Getter for the connection type
+     * </p>
+     * @return connection type
+     */
+    public final long getConnectionType() {
+        return connectionType.get();
+    }
+
+    /**<p>
+     * Setter for the connection type
+     * </p>
+     * @param value New packet count value
+     */
+    public final void setConnectionType(int value) {
+        connectionType.set(value);
+    }
+
+    /**<p>
+     * Getter for connection type property
+     * </p>
+     * @return connection type property
+     */
+    public IntegerProperty getConnectionTypeProperty() {
+        return connectionType;
+    }
 }
