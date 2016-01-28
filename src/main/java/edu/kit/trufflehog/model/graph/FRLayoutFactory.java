402cf5c872b6fe9f663b1811059b00b830079639
Jan Hermes <jan.hermes@student.kit.edu>
2016-01-28 11:27:10 +0100

    root -> jan

--- src/main/java/edu/kit/trufflehog/model/graph/FRLayoutFactory.java
+++ src/main/java/edu/kit/trufflehog/model/graph/FRLayoutFactory.java
@@ -3,7 +3,7 @@ package edu.kit.trufflehog.model.graph;
 import org.apache.commons.collections15.Transformer;
 
 /**
- * Created by root on 24.01.16.
+ * Created by Jan on 24.01.16.
  */
 public class FRLayoutFactory implements
         Transformer<ANetworkGraph, INetworkGraphLayout> {
