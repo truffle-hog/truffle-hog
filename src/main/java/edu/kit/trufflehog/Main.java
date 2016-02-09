f0d217e8cda49c434d0a8cff42c87facc64de67c
Julian Brendl <julianbrendl@gmail.com>
2016-02-09 21:59:31 +0100

    added log4j

--- src/main/java/edu/kit/trufflehog/Main.java
+++ src/main/java/edu/kit/trufflehog/Main.java
@@ -1,12 +1,17 @@
 package edu.kit.trufflehog;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * <p>
  *     The main class in TruffleHog. It initiates everything.
  * </p>
  */
 public class Main {
-    /**
+	private static final Logger logger = LogManager.getLogger(Main.class);
+
+	/**
 	 * <p>
 	 *     The main method of TruffleHog. It starts the program.
 	 * </p>
@@ -14,10 +19,13 @@ public class Main {
      * @param args command line arguments
      */
 	public static void main(String[] args) {
+        logger.debug("test");
 	}
 
     /**
      * Dummy method to test unit tests and code coverage
+     *
+     * TODO: Take away once real test exist for TruffleHog
      *
      * @param i any number
      * @return returns i
