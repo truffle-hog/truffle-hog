6851860114f4434d9e54950d55aabc41bc59892b
Mark Giraud <mark.giraud@student.kit.edu>
2016-02-15 16:48:30 +0100

    Added test for Truffle. Fixed some stuff in Truffle

---
+++ src/test/java/edu/kit/trufflehog/service/packetdataprocessor/profinetdataprocessor/TruffleTest.java
@@ -0,0 +1,33 @@
+package edu.kit.trufflehog.service.packetdataprocessor.profinetdataprocessor;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mark on 15.02.16.
+ */
+public class TruffleTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSetAttribute() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAttribute() throws Exception {
+
+    }
+}
