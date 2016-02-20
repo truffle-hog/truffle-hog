6f56b082cc0973416e4df25e01e9f9da0aabea2d
Julian Brendl <julianbrendl@gmail.com>
2016-02-20 18:28:03 +0100

    replaced anonymous inner classes with lambdas

--- src/main/java/edu/kit/trufflehog/presenter/Presenter.java
+++ src/main/java/edu/kit/trufflehog/presenter/Presenter.java
@@ -1,11 +1,6 @@
 package edu.kit.trufflehog.presenter;
 
-import edu.kit.trufflehog.Main;
-import edu.kit.trufflehog.command.usercommand.IUserCommand;
-import edu.kit.trufflehog.interaction.IInteraction;
 import edu.kit.trufflehog.view.*;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -16,9 +11,7 @@ import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import javax.management.InstanceAlreadyExistsException;
-
-import static edu.kit.trufflehog.Main.*;
+import static edu.kit.trufflehog.Main.getPrimaryStage;
 
 /**
  * <p>
@@ -66,7 +59,6 @@ public class Presenter {
         // setting up main window
         MainViewController mainView = new MainViewController("main_view.fxml");
         Scene mainScene = new Scene(mainView);
-        RootWindowController rootWindow = new RootWindowController(primaryStage, mainScene);
         primaryStage.setScene(mainScene);
         primaryStage.getIcons().add(
                 new Image(
@@ -74,13 +66,7 @@ public class Presenter {
         primaryStage.show();
 
         // CTRL+Q for program quitting
-        primaryStage.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN), new Runnable() {
-
-            @Override
-            public void run() {
-                primaryStage.close();
-            }
-        });
+        primaryStage.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN), primaryStage::close);
 
         // setting up general statistics overlay
         OverlayViewController generalStatisticsOverlay = new OverlayViewController("general_statistics_overlay.fxml");
@@ -91,46 +77,27 @@ public class Presenter {
         // setting up menubar
         Button settingsButton = new ImageButton("cog-3x.png");
         // TODO das ist ein bisschen hässlich. geht leider afaik nicht anders mit eigen gebauten javafx-nodes.
-        settingsButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                Stage settingsStage = new Stage();
-                SettingsViewController settingsView = new SettingsViewController("settings_view.fxml");
-                Scene settingsScene = new Scene(settingsView);
-                settingsStage.setScene(settingsScene);
-                settingsStage.show();
-                // CTRL+W for closing
-                settingsStage.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.W, KeyCombination.CONTROL_DOWN), new Runnable() {
-
-                    @Override
-                    public void run() {
-                        settingsStage.close();
-                    }
-                });
-                // CTRL+S triggers info about program settings saving
-                settingsStage.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN), new Runnable() {
-
-                    @Override
-                    public void run() {
-                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                        alert.setTitle("Relax, no need to save anything here");
-                        alert.setHeaderText(null);
-                        alert.setContentText("Oops. Seems you wanted to save the configuration bei pressing CTRL+S. This" +
-                                " is not necessary thanks to the awesome always up-to-date saving design of TruffleHog.");
-                        alert.showAndWait();
-                    }
-                });
-            }
+        settingsButton.setOnAction(event -> {
+            Stage settingsStage = new Stage();
+            SettingsViewController settingsView = new SettingsViewController("settings_view.fxml");
+            Scene settingsScene = new Scene(settingsView);
+            settingsStage.setScene(settingsScene);
+            settingsStage.show();
+            // CTRL+W for closing
+            settingsStage.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.W, KeyCombination.CONTROL_DOWN), settingsStage::close);
+            // CTRL+S triggers info about program settings saving
+            settingsStage.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN), () -> {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Relax, no need to save anything here");
+                alert.setHeaderText(null);
+                alert.setContentText("Oops. Seems you wanted to save the configuration bei pressing CTRL+S. This" +
+                        " is not necessary thanks to the awesome always up-to-date saving design of TruffleHog.");
+                alert.showAndWait();
+            });
         });
 
         // setting up keyboard shortcut
-        primaryStage.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.S, KeyCombination.ALT_DOWN), new Runnable() {
-
-            @Override
-            public void run() {
-                settingsButton.fire();
-            }
-        });
+        primaryStage.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.S, KeyCombination.ALT_DOWN), settingsButton::fire);
         MainToolBarController mainToolBarController = new MainToolBarController("main_toolbar.fxml", settingsButton);
         mainView.getChildren().add(mainToolBarController);
 
