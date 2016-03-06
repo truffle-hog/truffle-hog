b08c6cd39ddf32f322017db10aba4b119ee03748
Julian Brendl <julianbrendl@gmail.com>
2016-03-06 17:45:50 +0100

    got db to work

--- src/main/java/edu/kit/trufflehog/Debug.java
+++ src/main/java/edu/kit/trufflehog/Debug.java
@@ -2,6 +2,7 @@ package edu.kit.trufflehog;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
@@ -13,23 +14,63 @@ import java.sql.Statement;
  */
 public class Debug {
     public static void main(String[] args) {
-        Connection c = null;
-        Statement stmt = null;
+        Connection connection;
+        Statement statement;
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:./src/main/resources/db.sql");
+            connection = DriverManager.getConnection("jdbc:sqlite:./src/main/resources/db.sql");
             System.out.println("Opened database successfully");
 
-            stmt = c.createStatement();
-            String sql = "CREATE TABLE COMPANY " +
-                    "(ID INT PRIMARY KEY     NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " AGE            INT     NOT NULL, " +
-                    " ADDRESS        CHAR(50), " +
-                    " SALARY         REAL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
+            String sql;
+            statement = connection.createStatement();
+            sql = "CREATE TABLE FILTER" +
+                    "(ID  INTEGER PRIMARY KEY     AUTOINCREMENT ," +
+                    " ACTIVE          BOOLEAN BOOLEAN DEFAULT TRUE, " +
+                    " COLOR_R         INTEGER     NOT NULL," +
+                    " COLOR_G         INTEGER     NOT NULL, " +
+                    " COLOR_B         INTEGER     NOT NULL, " +
+                    " COLOR_A         INTEGER     NOT NULL)";
+            statement.executeUpdate(sql);
+
+            statement = connection.createStatement();
+             sql = "CREATE TABLE RULES" +
+                    "(ID  INTEGER PRIMARY KEY     NOT NULL," +
+                    " FILTER_ID       INTEGER     NOT NULL," +
+                    " RULE            TEXT    NOT NULL)";
+            statement.executeUpdate(sql);
+
+            sql = "INSERT INTO FILTER(COLOR_R,COLOR_G,COLOR_B,COLOR_A)" +
+                         "VALUES (0, 0, 0, 255);";
+            statement.executeUpdate(sql);
+
+            sql = "INSERT INTO FILTER(COLOR_R,COLOR_G,COLOR_B,COLOR_A)" +
+                    "VALUES (10, 10, 10, 255);";
+            statement.executeUpdate(sql);
+
+            sql = "INSERT INTO FILTER(COLOR_R,COLOR_G,COLOR_B,COLOR_A)" +
+                    "VALUES (20, 20, 20, 255);";
+            statement.executeUpdate(sql);
+
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM FILTER;");
+            while ( rs.next() ) {
+                int id = rs.getInt("id");
+                boolean active = rs.getBoolean("active");
+                int color_r  = rs.getInt("color_r");
+                int color_g  = rs.getInt("color_g");
+                int color_b  = rs.getInt("color_b");
+                int color_a  = rs.getInt("color_a");
+                System.out.println("ID = " + id);
+                System.out.println("ACTIVE = " + active);
+                System.out.println("COLOR_R = " + color_r);
+                System.out.println("COLOR_G = " + color_g);
+                System.out.println("COLOR_B = " + color_b);
+                System.out.println("COLOR_A = " + color_a);
+                System.out.println();
+            }
+
+            statement.close();
+            connection.close();
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
