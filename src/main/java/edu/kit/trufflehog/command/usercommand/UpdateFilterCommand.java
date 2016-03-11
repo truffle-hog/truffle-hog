212bb5bcfaa53db012adc4dc1982325b0b208a56
Valentin <unekn@student.kit.edu>
2016-03-11 15:16:58 +0100

    added command-filter functionality then realized the view controllers already do this

--- src/main/java/edu/kit/trufflehog/command/usercommand/UpdateFilterCommand.java
+++ src/main/java/edu/kit/trufflehog/command/usercommand/UpdateFilterCommand.java
@@ -1,35 +1,37 @@
 package edu.kit.trufflehog.command.usercommand;
 
+import edu.kit.trufflehog.model.configdata.ConfigDataModel;
+import edu.kit.trufflehog.model.filter.FilterInput;
 import edu.kit.trufflehog.model.filter.IFilter;
 import edu.kit.trufflehog.model.network.INetworkWritingPort;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 /**
  * <p>
- *     Command used to run filters on a graph. Can be used to update the graph after filter preferences have changed.
+ *     Command to add a filter created in GUI to the actual data base
  * </p>
  */
 public class UpdateFilterCommand implements IUserCommand<Void> {
-
-    private final INetworkWritingPort network;
-    private final List<IFilter> filterList;
+    private final ConfigDataModel cdm;
+    private final FilterInput fi;
 
     /**
      * <p>
-     *     Creates new command, provides a graph to work on and the filters to check.
+     *     Constructor taking the given informations in form of a FilterInput
      * </p>
-     * @param port {@link INetworkWritingPort} to write data to
-     * @param filters List of filters to check
+     * @param fi filter input to create a filter out of
+     * @param cdm ConfigDataModel to add filter to
      */
-    UpdateFilterCommand(final INetworkWritingPort port, final List<IFilter> filters) {
-        network = port;
-        filterList = filters;
+    UpdateFilterCommand(final ConfigDataModel cdm, final FilterInput fi) {
+        this.fi = fi;
+        this.cdm = cdm;
     }
 
     @Override
     public void execute() {
-
+        cdm.addFilterInput(fi);
     }
 
     @Override
