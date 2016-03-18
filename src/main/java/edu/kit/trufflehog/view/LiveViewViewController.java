9e0497af6ce62cda665cbd5986057268bf52076f
Julian Brendl <julianbrendl@gmail.com>
2016-03-18 11:32:36 +0100

    removed settings button

--- src/main/java/edu/kit/trufflehog/view/LiveViewViewController.java
+++ src/main/java/edu/kit/trufflehog/view/LiveViewViewController.java
@@ -233,14 +233,14 @@ public class LiveViewViewController extends AnchorPaneInteractionController<Prot
      * </p>
      */
     private void addToolbar() {
-        final Button settingsButton = addSettingsButton();
+        //final Button settingsButton = addSettingsButton();
         final Button filterButton = addFilterButton();
         //final Button recordButton = addRecordButton();
         final Button connectButton = addConnectButton();
 
 //        final ToolBarViewController mainToolBarController = new ToolBarViewController("main_toolbar.fxml", settingsButton,
 //                filterButton, recordButton, connectButton);
-        final ToolBarViewController mainToolBarController = new ToolBarViewController("main_toolbar.fxml", settingsButton,
+        final ToolBarViewController mainToolBarController = new ToolBarViewController("main_toolbar.fxml",
                 filterButton, connectButton);
         this.getChildren().add(mainToolBarController);
         AnchorPane.setBottomAnchor(mainToolBarController, 5d);
