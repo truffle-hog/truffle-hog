20e2b85b68e1e62ece3bb16d510074f622338fb4
Jan Hermes <jan.hermes@student.kit.edu>
2016-03-25 08:54:28 +0100

    some changes i dont know which now

--- src/main/java/edu/kit/trufflehog/presenter/Presenter.java
+++ src/main/java/edu/kit/trufflehog/presenter/Presenter.java
@@ -21,6 +21,7 @@ import edu.kit.trufflehog.service.NodeStatisticsUpdater;
 import edu.kit.trufflehog.service.executor.CommandExecutor;
 import edu.kit.trufflehog.service.packetdataprocessor.profinetdataprocessor.TruffleCrook;
 import edu.kit.trufflehog.service.packetdataprocessor.profinetdataprocessor.TruffleReceiver;
+import edu.kit.trufflehog.service.packetdataprocessor.profinetdataprocessor.UnixSocketReceiver;
 import edu.kit.trufflehog.view.ViewSwitcher;
 import edu.kit.trufflehog.view.jung.visualization.FXVisualizationViewer;
 import edu.kit.trufflehog.view.jung.visualization.SceneGestures;
@@ -201,8 +202,8 @@ public class Presenter {
         final ExecutorService truffleFetchService = Executors.newSingleThreadExecutor();
 
         // TODO register the truffleReceiver somewhere so we can start or stop it.
-        //truffleReceiver = new UnixSocketReceiver(liveNetwork.getWritingPort(), macroFilter);
-       truffleReceiver = new TruffleCrook(liveNetwork.getWritingPort(), macroFilter);
+        truffleReceiver = new UnixSocketReceiver(liveNetwork.getWritingPort(), macroFilter);
+      // truffleReceiver = new TruffleCrook(liveNetwork.getWritingPort(), macroFilter);
 
 
 
