b24ec2c0496735cf70a89c7d6251aa83db0493bb
Valentin <unekn@student.kit.edu>
2016-04-06 23:04:43 +0200

    updatefilter test template

---
+++ src/test/java/edu/kit/trufflehog/command/usercommand/UpdateFilterCommandTest.java
@@ -0,0 +1,10 @@
+package edu.kit.trufflehog.command.usercommand;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Valentin Kiechle on 06.04.2016.
+ */
+public class UpdateFilterCommandTest {
+
+}
