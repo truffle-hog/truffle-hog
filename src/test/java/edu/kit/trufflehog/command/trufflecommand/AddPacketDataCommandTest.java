35578d96fbcf2b3b053835e4a6fa9045a45a961b
Valentin <unekn@student.kit.edu>
2016-04-06 18:20:18 +0200

    copy paste tests

--- src/test/java/edu/kit/trufflehog/command/trufflecommand/AddPacketDataCommandTest.java
+++ src/test/java/edu/kit/trufflehog/command/trufflecommand/AddPacketDataCommandTest.java
@@ -2,9 +2,7 @@ package edu.kit.trufflehog.command.trufflecommand;
 
 
 import edu.kit.trufflehog.model.filter.IFilter;
-import edu.kit.trufflehog.model.network.INetworkWritingPort;
-import edu.kit.trufflehog.model.network.MacAddress;
-import edu.kit.trufflehog.model.network.NetworkIOPort;
+import edu.kit.trufflehog.model.network.*;
 import edu.kit.trufflehog.model.network.graph.IConnection;
 import edu.kit.trufflehog.model.network.graph.INode;
 import edu.kit.trufflehog.service.packetdataprocessor.IPacketData;
@@ -13,11 +11,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import java.util.logging.Filter;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Valentin Kiechle on 12.02.2016.
@@ -33,8 +30,12 @@ public class AddPacketDataCommandTest {
         writingPort = mock(NetworkIOPort.class);
         filter = mock(IFilter.class);
         data = mock(Truffle.class);
+        when(data.toString()).thenReturn("dataString");
         when(data.getAttribute(MacAddress.class, "sourceMacAddress")).thenReturn(new MacAddress(1L));
         when(data.getAttribute(MacAddress.class, "destMacAddress")).thenReturn(new MacAddress(2L));
+        when(data.getAttribute(String.class, "deviceName")).thenReturn("device1");
+        when(data.getAttribute(IPAddress.class, "sourceIPAddress")).thenReturn(new IPAddress(42L));
+        when(data.getAttribute(Boolean.class, "isResponse")).thenReturn(true);
     }
     @After
     public void teardown() {
@@ -44,11 +45,18 @@ public class AddPacketDataCommandTest {
     }
 
     @Test
-    public void addPacketDataCommandTest() {
+    public void addOnePacketTest() {
         AddPacketDataCommand apdc = new AddPacketDataCommand(writingPort, data, filter);
         apdc.execute();
         verify(writingPort, times(2)).writeNode(any(INode.class));
         verify(writingPort, times(1)).writeConnection(any(IConnection.class));
-       // verify(any(INode.class), times(2)).getComposition().addComponent(any(MulticastNodeRendererComponent.class));
+        // verify(any(INode.class), times(2)).getComposition().addComponent(any(MulticastNodeRendererComponent.class);
+    }
+
+    @Test
+    public void toStringTest() {
+        AddPacketDataCommand apdc = new AddPacketDataCommand(writingPort, data, filter);
+        apdc.toString();
+        assertEquals("dataString", apdc.toString());
     }
 }
